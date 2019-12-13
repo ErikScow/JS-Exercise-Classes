3cec64ed007d805143565610cdaebe758ee8a228
Erik Scow <erikscow@gmail.com>
2019-12-12 19:57:33 -0800

    completed Instructor class

--- index.js
+++ index.js
@@ -114,7 +114,15 @@ class Car {
         + {name} and {location} of course come from the instance's own properties.
 */
 class Lambdasian {
+  constructor(info){
+    this.name = info.name;
+    this.age = info.age;
+    this.location = info.location;
+  }
 
+  speak(){
+    return `Hello my name is ${this.name}, I am from ${this.location}`;
+  }
 }
 
 /*
@@ -131,8 +139,21 @@ class Lambdasian {
         + `demo` receives a `subject` string as an argument and returns the phrase 'Today we are learning about {subject}' where subject is the param passed in.
         + `grade` receives a `student` object and a `subject` string as arguments and returns '{student.name} receives a perfect score on {subject}'
 */
-class Instructor {
+class Instructor extends Lambdasian{
+  constructor(info){
+    super(info);
+    this.specialty = info.specialty;
+    this.favLanguage = info.favLanguage;
+    this.catchPhrase = info.catchPhrase;
+  }
 
+  demo(subject){
+    return `Today we are learning about ${subject}`
+  }
+
+  grade(student, subject){
+    return `${student.name} recieves a perfect score on ${subject}`;
+  }
 }
 
 /*
